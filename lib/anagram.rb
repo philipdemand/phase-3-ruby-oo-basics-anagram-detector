ee5462637dc2a627381e50deb78b79349e929e42
Philip Demand <philipdemand@yahoo.com>
2023-06-24 14:25:25 -0400

    commit tree 6d4d76f1c9e08de4d911fbc2285b5e755ade1d6c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+  
+    def initialize(word)
+      @word = word
+    end
+
+    def match(array)
+      word_chars = self.word.downcase.chars.sort
+      match_array = []
+      array.each do |w|
+        w_chars = w.downcase.chars.sort
+      if w_chars == word_chars
+        match_array.push(w)
+      end
+    end
+      match_array
+  end
+  
+end
